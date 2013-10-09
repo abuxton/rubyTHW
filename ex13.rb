3b31cff63a084fbcb35df995130c21dc3b409cee
abuxton <buxton.adam@gmail.com>
2013-10-09 11:43:19 +0100

    STDIN to var

---
+++ ex13.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+#http://ruby.learncodethehardway.org/book/ex13.html
+
+first, second, third = ARGV 
+
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+one, two, three, four = ARGV
+puts "a test : #{$1}"
+fourth = ARGV[4]
+puts "a second test : #{fourth} "
+puts "final test : #{four}"
