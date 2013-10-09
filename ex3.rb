0a5802615378b61168dbdf5d354750d1ce8671ec
abuxton <buxton.adam@gmail.com>
2013-10-09 11:19:19 +0100

    Libraries

--- ex3.rb
+++ ex3.rb
@@ -1,14 +1,31 @@
 #!/usr/bin/ruby
 #http://ruby.learncodethehardway.org/book/ex3.html
+include Math
 
 puts "I will now count my chickens:"
 
-puts "Hens", 25 + 30 / 6
-puts "Roosters", 100 - 25 * 3 % 4
+puts "Hens", 25 + 30 / 6 #prints hens, then a new line the sum of (25 + 30/6) as no concasternation it sees , as command seperator so puts x,y on seperate lines
+
+puts "Roosters", 100 - 25 * 3 % 4 #as above ruby does logical sum partitioning eg it automatcially compute sthis as 2 halves either side of teh central operand
 
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#theFloat =  3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "the origional", 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts "simply loated", Float( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+puts "with the math operation precedence controlled", (((( 3 + 2 + 1 ) - 5 ) + (4%2)) - 1 ) / ( 4 + 6 )
+puts "the new"
+theFloat= 3 + 2 + 1 + 4
+puts theFloat
+theFloat = theFloat - 5
+puts theFloat
+theFloat = theFloat % 2
+puts theFloat
+theFloat = theFloat -1
+puts theFloat
+theFloat = Float(theFloat)/10
+puts theFloat
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
